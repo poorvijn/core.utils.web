a08d9dad40abcdd3f88da0b5f4428e5507f153cb
Poorvi J Nayak <87180220+poorvijn@users.noreply.github.com>
2025-05-02 17:29:17 +0530

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,13 @@
+# ---------------- READ THIS -----------------------------------
+#
+#          DO NOT MODIFY ANY CODE IN THIS MODULE
+#
+# This is a namespace package and should not have an additional
+# code added beyond what is already here. Any additional code
+# can cause issues with other components using the same namespace
+# package.
+# For more info:
+# https://packaging.python.org/en/latest/guides/packaging-namespace-packages/#pkgutil-style-namespace-packages
+# --------------------------------------------------------------
+
+__path__ = __import__('pkgutil').extend_path(__path__, __name__)
